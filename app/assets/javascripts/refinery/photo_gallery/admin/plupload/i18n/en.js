ffdfa67e0a7eb3bf6760e49e2ad9b2c9a753bf45
borischernov <borisch@inbox.lv>
2014-02-02 03:06:50 +0200

    Update en.js

--- app/assets/javascripts/refinery/photo_gallery/admin/plupload/i18n/en.js
+++ app/assets/javascripts/refinery/photo_gallery/admin/plupload/i18n/en.js
@@ -1,2 +1,25 @@
 // English (en)
-plupload.addI18n({"Stop Upload":"Stop Upload","Upload URL might be wrong or doesn't exist.":"Upload URL might be wrong or doesn't exist.","tb":"tb","Size":"Size","Close":"Close","Init error.":"Init error.","Add files to the upload queue and click the start button.":"Add files to the upload queue and click the start button.","Filename":"Filename","Image format either wrong or not supported.":"Image format either wrong or not supported.","Status":"Status","HTTP Error.":"HTTP Error.","Start Upload":"Start Upload","mb":"mb","kb":"kb","Duplicate file error.":"Duplicate file error.","File size error.":"File size error.","N/A":"N/A","gb":"gb","Error: Invalid file extension:":"Error: Invalid file extension:","Select files":"Select files","%s already present in the queue.":"%s already present in the queue.","File: %s":"File: %s","b":"b","Uploaded %d/%d files":"Uploaded %d/%d files","Upload element accepts only %d file(s) at a time. Extra files were stripped.":"Upload element accepts only %d file(s) at a time. Extra files were stripped.","%d files queued":"%d files queued","File: %s, size: %d, max file size: %d":"File: %s, size: %d, max file size: %d","Drag files here.":"Drag files here.","Runtime ran out of available memory.":"Runtime ran out of available memory.","File count error.":"File count error.","File extension error.":"File extension error.","Error: File too large:":"Error: File too large:","Add Files":"Add Files"});
+plupload.addI18n({
+'Select files' : 'Select files',
+'Add files to the upload queue and click the start button.' : 'Add files to the upload queue and click the start button.',
+'Filename' : 'Filename',
+'Status' : 'Status',
+'Size' : 'Size',
+'Add files' : 'Add files',
+'Stop current upload' : 'Stop current upload',
+'Start uploading queue' : 'Start uploading queue',
+'Uploaded %d/%d files': 'Uploaded %d/%d files',
+'N/A' : ''N/A',
+'Drag files here.' : 'Drag files here.',
+'File extension error.': 'File extension error.',
+'File size error.': 'File size error.',
+'Init error.': 'Init error.',
+'HTTP Error.': 'HTTP Error.',
+'Security error.': 'Security error.',
+'Generic error.': 'Generic error.',
+'IO error.': 'IO error.',
+'Stop Upload': 'Stop Upload',
+'Add Files': 'Add Files',
+'Start Upload': 'Start Upload',
+'%d files queued': '%d files queued'
+});
